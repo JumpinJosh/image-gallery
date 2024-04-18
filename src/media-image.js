24f0c274c70ebdb137cc6fbde2ab97be737c1e51
Joshua Schwartzman <jas9049@psu.edu>
2024-04-18 13:26:53 -0400

    Trying to get openDialog to work

--- src/media-image.js
+++ src/media-image.js
@@ -1,5 +1,8 @@
 import { html, css} from "lit";
 import { DDD } from "@lrnwebcomponents/d-d-d/d-d-d.js";
+//import { PlayList } from "@lrnwebcomponents/play-list/play-list.js";
+
+
 
 export class MediaImage extends DDD {
     static get tag() {
@@ -47,23 +50,61 @@ export class MediaImage extends DDD {
         }
 
         .description {
-          visibility: hidden;
+          display: none;
         }
         `
       ];
+    }
+
+    openDialog() {
+      const background = document.querySelector("transparent-background");
+      const img = document.querySelector("transparent-background + img");
+      const closeButton = document.querySelector("transparent-background close-button")
+
+      const event = new CustomEvent("dialog-opened", {
+        bubbles: true,
+        composed: true,
+        cancelable: true,
+        detail: {
+          opened: true,
+        },
+      });
+
+      this.dispatchEvent(event);
+      
+      if (event.detail.opened == "false") {
+        img.addEventListener("click", () => {
+          background.showModal();
+        });
+      }
+      else {
+        closeButton.addEventListener("click", () => {
+          background.close();
+        });
+      }
     }
     
     render() {
       return html`
       <div class="background">
         <div class="image-wrapper">
-          <img src="https://th.bing.com/th/id/OIP.PDlm3trgAkY6pGPcbRt4SQHaEK?w=289&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7" alt="funny-dog.png">
+          <img src="https://th.bing.com/th/id/OIP.PDlm3trgAkY6pGPcbRt4SQHaEK?w=289&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7" alt="funny-dog.png" @click=${this.openDialog}>
         </div>
         <div class="text-wrapper">
           <p class="Caption">${this.caption}</p>
           <p class="Description">${this.description}</p>
         </div>
       </div>
+
+      <dialog class="transparent-background">
+        <button class="back-button"></button>
+        <p class="num-indicater">1 of 10</p>
+        <div class="image-wrapper">
+          <img src="https://th.bing.com/th/id/OIP.PDlm3trgAkY6pGPcbRt4SQHaEK?w=289&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7" alt="funny-dog.png">
+        </div>
+        <button class="forward-button"></button>
+        <button class="close-button"></button>
+      </dialog>
       `;
     }
     
